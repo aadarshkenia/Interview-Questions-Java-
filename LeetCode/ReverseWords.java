01a1cfc8326cf498e7b7c1e208cb9cf2afbe7a08
Aadarsh Kenia <aadarsh.kenia@stonybrook.edu>
2015-12-01 06:13:33 -0500

    Dec-1 2015

---
+++ LeetCode/ReverseWords.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+class ReverseWords{
+
+	public static void main(String args[]){
+
+		ReverseWords obj = new ReverseWords();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		String input = null;
+		try{
+			input = br.readLine();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		String output = obj.reverse(input); 
+		System.out.println(output);
+		System.out.println("Ans len: "+output.length());	
+	}
+
+
+
+	public String reverse(String input){
+
+		if(input == null)
+			return null;
+		int len = input.length();
+		StringBuilder output = new StringBuilder();
+		int last_space = len;
+		for(int i=len-1;i>=0;i--){
+			if(input.charAt(i)==' ' || i==0){
+				if(last_space == i+1 && input.charAt(i)== ' '){
+					last_space = i;
+					continue;
+				}
+				
+				String current_word = "";
+				if(i!=0){
+					current_word = input.substring(i+1, last_space);
+				}
+				else{
+					if(input.charAt(i)!=' ')
+						current_word = input.substring(0, last_space);
+					else 
+						current_word = input.substring(1, last_space);
+				}
+				if(output.length()!=0)
+					output.append(" ");
+				output.append(current_word);
+				last_space = i;
+			} 
+		}
+
+		return output.toString();
+
+
+	}
+
+}
--- Leetcode OJ/Test.java
+++
@@ -1,8 +0,0 @@
-class Test
-{
-    public static void main(String args[])
-    {
-      	int i = 97;
-                System.out.println((Character)(i+1));
-    }
-}
